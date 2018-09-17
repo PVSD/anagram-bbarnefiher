8fc0ddbffba920c20df8a8d978abc427af18c5ce
bbarnefiher <bb222@pvsd.org>
2018-09-17 11:12:39 -0400

    middle of class btw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner stringScanner = new Scanner(System.in);
+        System.out.println("Anagram Test\nPlease enter your two phrases on different lines.\nPhrase One:");
+        String phraseOne = stringScanner.nextLine();
+        System.out.println("Phrase Two:");
+        String phraseTwo = stringScanner.nextLine();
+
+
+        if (phraseOne.length() == phraseTwo.length()){
+
+            
+
+
+            for (int i = 0; i < phraseOne.length(); i++){
+                System.out.println(i);
+
+
+
+
+            }
+
+            //creates array of both strings with one character per index
+
+        }
+        else {
+            System.out.println("Your two phrases are not anagrams of each other.");
+            System.exit(0);
+        }
+
+
+
+
+
+
+
     }
 }
